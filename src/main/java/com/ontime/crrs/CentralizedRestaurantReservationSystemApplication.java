e2562ad19962018ac46049845a0b56226051d104
Selma Čopra <selmacopra27@gmail.com>
2023-03-11 22:17:57 +0100

    Project setup

---
+++ src/main/java/com/ontime/crrs/CentralizedRestaurantReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ontime.crrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedRestaurantReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedRestaurantReservationSystemApplication.class, args);
+	}
+
+}
