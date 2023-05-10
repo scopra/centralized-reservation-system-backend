46bd468a845529f302173de2d123b8d4d75baf2e
Selma Čopra <selmacopra27@gmail.com>
2023-05-10 16:21:38 +0200

    adding rule entity

---
+++ src/main/java/com/ontime/crrs/business/mapper/rule/RuleMapper.java
@@ -0,0 +1,27 @@
+package com.ontime.crrs.business.mapper.rule;
+
+import com.ontime.crrs.business.mapper.restaurant.RestaurantMapper;
+import com.ontime.crrs.business.rule.model.Rule;
+import com.ontime.crrs.persistence.rule.entity.RuleEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(
+        uses = RestaurantMapper.class,
+        componentModel = "spring"
+)
+public interface RuleMapper {
+
+    RuleMapper INSTANCE = Mappers.getMapper(RuleMapper.class);
+
+    Rule entityToModel(RuleEntity entity);
+
+    RuleEntity modelToEntity(Rule model);
+
+    List<Rule> entitiesToModels(List<RuleEntity> entities);
+
+    List<RuleEntity> modelsToEntities(List<Rule> models);
+
+}
--- src/main/java/com/ontime/crrs/business/rule/controller/RuleController.java
+++ src/main/java/com/ontime/crrs/business/rule/controller/RuleController.java
@@ -1,9 +1,10 @@
 package com.ontime.crrs.business.rule.controller;
 
+import com.ontime.crrs.business.mapper.rule.RuleMapper;
 import com.ontime.crrs.business.rule.model.Rule;
+import com.ontime.crrs.persistence.restaurant.service.RestaurantService;
 import com.ontime.crrs.persistence.rule.service.RuleService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,15 +16,28 @@ import java.util.UUID;
 public class RuleController {
 
     private final RuleService ruleService;
+    private final RestaurantService restaurantService;
+    private final RuleMapper mapper;
 
     @PostMapping("/{restaurantName}")
     public ResponseEntity<?> addNewRule(@PathVariable String restaurantName, @RequestBody Rule rule) {
-        return null;
+        var restaurant = restaurantService.findRestaurantByName(restaurantName);
+
+        var ruleEntity = mapper.modelToEntity(rule);
+        ruleEntity.setRestaurant(restaurant);
+
+        var savedRule = mapper.entityToModel(ruleService.updateRule(ruleEntity));
+
+        return ResponseEntity.ok(savedRule);
     }
 
     @GetMapping("/{restaurantName}")
-    public CollectionModel<?> getRulesForRestaurant(@PathVariable String restaurantName) {
-        return null;
+    public ResponseEntity<?> getRulesForRestaurant(@PathVariable String restaurantName) {
+        var restaurant = restaurantService.findRestaurantByName(restaurantName);
+
+        var ruleList = mapper.entitiesToModels(ruleService.findRulesByRestaurant(restaurantName));
+
+        return ResponseEntity.ok(ruleList);
     }
 
     @PutMapping("/{restaurantName}")
@@ -33,8 +47,11 @@ public class RuleController {
 
     @DeleteMapping("/owner/{ruleId}")
     public ResponseEntity<?> deleteRule(@PathVariable UUID ruleId) {
+        ruleService.deleteRuleById(ruleId);
+
         return ResponseEntity
                 .noContent()
                 .build();
     }
+
 }
